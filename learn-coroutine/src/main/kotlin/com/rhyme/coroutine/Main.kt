47903adc0112120c80985e09073344d5a6865808
LeeChunJun <1772043675@qq.com>
2022-10-16 23:24:55 +0800

    [FEATURE]增加携程测试类1

--- learn-coroutine/src/main/kotlin/com/rhyme/coroutine/Main.kt
+++ learn-coroutine/src/main/kotlin/com/rhyme/coroutine/Main.kt
@@ -1,6 +1,23 @@
 package com.rhyme.coroutine
 
+import kotlin.coroutines.Continuation
+import kotlin.coroutines.EmptyCoroutineContext
+import kotlin.coroutines.createCoroutine
+
 fun main() {
-    println("ccc")
+    val continuation = suspend {
+        println("In Coroutine")
+        5
+    }.createCoroutine(object : Continuation<Int> {
+        override fun resumeWith(result: Result<Int>) {
+            println("Coroutine End: $result")
+        }
+
+        override val context: EmptyCoroutineContext
+            get() {
+                TODO()
+            }
+    })
+
 
 }
